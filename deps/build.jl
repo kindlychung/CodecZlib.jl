a0ec01864a30cecdb2879a4adf5a8e653571e186
Kaiyin Zhong <kindlychung@gmai.com>
2019-07-06 16:46:14 +0200

    fix typo

--- deps/build.jl
+++ deps/build.jl
@@ -4,7 +4,7 @@ using Libdl
 # Parse some basic command-line arguments
 const verbose = "--verbose" in ARGS
 const prefix = Prefix(get([a for a in ARGS if a != "--verbose"], 1, joinpath(@__DIR__, "usr")))
-@info "####Prefix: prefix"
+@info "####Prefix: $prefix"
 mkpath(prefix)
 products = [
     LibraryProduct(prefix, ["libz"], :libz),
